a84fbe8c4d08498d69d912e5b9c5d79a95b61541
Marianne K <marianne.hannus@tktk.ee>
2024-10-21 21:53:11 +0300

    add week4loops code

---
+++ RandomNumber/Program.cs
@@ -0,0 +1,18 @@
+﻿Random rnd = new Random();
+
+
+int myRandomNum;
+
+int randomSum = 0;
+for (int i = 0; i < 3; i++)
+{
+    myRandomNum = rnd.Next(0, 11); //genereeritakse väärtused 0-10
+    randomSum = randomSum + myRandomNum;
+    Console.WriteLine($"My random number is: {myRandomNum}");
+}
+
+Console.WriteLine($"random sum total: {randomSum}");
+
+
+
+
